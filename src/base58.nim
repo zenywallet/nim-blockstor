e7cbdc7bc9e0a189facdc2840aa4fcb6a3d38ef6
zenywallet <zenywallet@bitzeny.jp>
2021-04-13 11:39:48 +0900

    base58 optimization

--- src/base58.nim
+++ src/base58.nim
@@ -1,5 +1,7 @@
 # Copyright (c) 2020 zenywallet
 
+import math
+
 const base58Chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 var base58Map = [int8 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
                 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -10,44 +12,139 @@ var base58Map = [int8 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
                 -1, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, -1, 44, 45, 46,
                 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, -1, -1, -1, -1]
 
+const log58_256 = 1.365658237309762
+const log256_58 = 0.7322476243909466
+
 proc enc*(data: seq[byte]): string =
+  let size = int(ceil(data.len.float * log58_256))
+  var b = newSeqUninitialized[byte](size)
+  var zeroLen = 0
+
   for d in data:
     if d != 0:
       break
-    result.add('1')
-  var b: seq[byte]
-  for d in data[result.len..data.high]:
+    b[zeroLen] = '1'.byte
+    inc(zeroLen)
+
+  var pos = b.len
+  for d in data[zeroLen..data.high]:
     var c = d.int
-    var j = 0
-    while c > 0 or j < b.len:
-      if j >= b.len:
-        b.add(0)
+    var j = b.high
+
+    while c > 0 or j >= pos:
+      if j < pos:
+        dec(pos)
+        b[pos] = 0'u8
       else:
         c = c + b[j].int * 256
       b[j] = (c mod 58).byte
       c = c div 58
-      inc(j)
-  for i in countdown(b.high, 0):
-    result.add(base58Chars[b[i]])
+      dec(j)
+
+  let validLen = b.len - pos
+  let bLen = zeroLen + validLen
+  let offset = pos - zerolen
+  for i in zeroLen..<bLen:
+    b[i] = base58Chars[b[i + offset]].byte
+  b.setLen(bLen)
+  result = cast[string](cast[seq[char]](b))
 
 proc dec*(data: string): seq[byte] =
+  let size = int(ceil(data.len.float * log256_58)) + 1
+  var b = newSeqUninitialized[byte](size)
+  var zeroLen = 0
+
   for d in data:
     if d != '1':
       break
-    result.add(0'u8)
-  var b: seq[byte]
-  for d in data[result.len..data.high]:
+    b[zeroLen] = 0'u8
+    inc(zeroLen)
+
+  var pos = b.len
+  for d in data[zeroLen..data.high]:
     var idx = cast[int8](d)
     if idx < 0:
       return
     var c = base58Map[idx].int
     if c == -1:
       return
-    for j in 0..b.high:
+    for j in countdown(b.high, pos):
       c = c + b[j].int * 58
       b[j] = (c mod 256).byte
       c = c div 256
     if c > 0:
-      b.add(c.byte)
-  for i in countdown(b.high, 0):
-    result.add(b[i])
+      dec(pos)
+      b[pos] = c.byte
+
+  if pos > zeroLen:
+    let validLen = b.len - pos
+    let bLen = zeroLen + validLen
+    copyMem(addr b[zeroLen], addr b[pos], validLen)
+    b.setLen(bLen)
+  result = b
+
+
+when isMainModule:
+  proc enc_reference*(data: seq[byte]): string =
+    for d in data:
+      if d != 0:
+        break
+      result.add('1')
+    var b: seq[byte]
+    for d in data[result.len..data.high]:
+      var c = d.int
+      var j = 0
+      while c > 0 or j < b.len:
+        if j >= b.len:
+          b.add(0)
+        else:
+          c = c + b[j].int * 256
+        b[j] = (c mod 58).byte
+        c = c div 58
+        inc(j)
+    for i in countdown(b.high, 0):
+      result.add(base58Chars[b[i]])
+
+  proc dec_reference*(data: string): seq[byte] =
+    for d in data:
+      if d != '1':
+        break
+      result.add(0'u8)
+    var b: seq[byte]
+    for d in data[result.len..data.high]:
+      var idx = cast[int8](d)
+      if idx < 0:
+        return
+      var c = base58Map[idx].int
+      if c == -1:
+        return
+      for j in 0..b.high:
+        c = c + b[j].int * 58
+        b[j] = (c mod 256).byte
+        c = c div 256
+      if c > 0:
+        b.add(c.byte)
+    for i in countdown(b.high, 0):
+      result.add(b[i])
+
+
+  import times, sequtils
+
+  var d = cast[seq[byte]]("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".toSeq)
+  var a1 = epochTime()
+  for i in 0..<10000:
+    discard dec_reference(enc_reference(d))
+  var a2 = epochTime()
+  for i in 0..<10000:
+    discard dec_reference(enc(d))
+  var a3 = epochTime()
+  for i in 0..<10000:
+    discard dec(enc_reference(d))
+  var a4 = epochTime()
+  for i in 0..<10000:
+    discard dec(enc(d))
+  var a5 = epochTime()
+  echo a2 - a1
+  echo a3 - a2
+  echo a4 - a3
+  echo a5 - a4
