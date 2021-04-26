91db9ae89038f978ed6e9f26a89caf21598fa650
zenywallet <zenywallet@bitzeny.jp>
2021-04-26 10:10:58 +0900

    add seed native

---
+++ src/seed.nim
@@ -0,0 +1,15 @@
+# Copyright (c) 2021 zenywallet
+
+include seed_native
+
+proc cryptSeed*(buf: ptr UncheckedArray[byte], size: int): int {.importc: "crypt_seed".}
+
+when not defined(emscripten):
+  proc cryptSeed*(buf: openArray[byte]): int {.inline.} =
+    cryptSeed(cast[ptr UncheckedArray[byte]](buf), buf.len)
+
+  proc cryptSeed*(size: int): seq[byte] =
+    var a = newSeq[byte](size)
+    var ret = cryptSeed(cast[ptr UncheckedArray[byte]](addr a[0]), size)
+    if ret == 0:
+      result = a
---
+++ src/seed_native.nim
@@ -0,0 +1,64 @@
+when defined(emscripten):
+  proc crypt_seed*(seed: pointer, size: int): int {.exportc.} =
+    {.emit:"""
+#include <emscripten.h>
+
+int ret = EM_ASM_INT(
+try {
+  if(crypto && crypto.getRandomValues) {
+    var seed = new Uint8Array($1);
+    crypto.getRandomValues(seed);
+    HEAPU8.set(seed, $0);
+    return 0;
+  }
+} catch(e) {}
+try {
+   if(!global.crypto) {
+    global.crypto = require('crypto');
+  }
+  var seed = crypto.randomBytes($1);
+  HEAPU8.set(seed, $0);
+  return 0;
+} catch(e) {
+  return 1;
+}
+, seed, size);
+return ret;
+""".}
+else:
+  {.emit: """
+#ifdef _WIN32
+#include <windows.h>
+#include <wincrypt.h>
+#else
+#include <stdio.h>
+#endif
+
+int crypt_seed(unsigned char *seed, int size) {
+#ifdef _WIN32
+  HCRYPTPROV prov;
+
+  if (!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))  {
+    return 1;
+  }
+
+  if (!CryptGenRandom(prov, size, seed))  {
+    CryptReleaseContext(prov, 0);
+    return 1;
+  }
+
+  CryptReleaseContext(prov, 0);
+#else
+  FILE *f = fopen("/dev/urandom", "rb");
+
+  if (f == NULL) {
+    return 1;
+  }
+
+  fread(seed, 1, size, f);
+  fclose(f);
+#endif
+
+  return 0;
+}
+"""}
