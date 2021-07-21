c5e0bf367928e0c246557def7469f4247aeffd7d
zenywallet <zenywallet@bitzeny.jp>
2021-07-21 15:42:54 +0900

    separate bip32 and eckey

--- src/bip32.nim
+++ src/bip32.nim
@@ -4,7 +4,7 @@ import bytes
 import base58
 import nimcrypto
 import sequtils
-import secp256k1
+import eckey
 import utils
 import address
 
@@ -14,9 +14,6 @@ const VersionTestnetPublic* = 0x043587CF'u32
 const VersionTestnetPrivate* = 0x04358394'u32
 
 type
-  PrivateKey* = distinct seq[byte]
-  PublicKey* = distinct seq[byte]
-  PublicKeyObj* = distinct seq[byte]
   ChainCode* = distinct seq[byte]
 
   HDNode* = ref object
@@ -29,122 +26,11 @@ type
     versionPub: uint32
     versionPrv: uint32
 
-  EcError* = object of CatchableError
   HdError* = object of CatchableError
 
 
-converter toBytes*(o: PrivateKey | PublicKey | PublicKeyObj | ChainCode): seq[byte] {.inline.} =
-  cast[seq[byte]](o)
-converter toPrivateKey*(s: seq[byte]): PrivateKey {.inline.} = PrivateKey(s)
-converter toPublicKey*(s: seq[byte]): PublicKey {.inline.} = PublicKey(s)
-converter toPublicKeyObj*(s: seq[byte]): PublicKeyObj {.inline.} = PublicKeyObj(s)
+converter toBytes*(o: ChainCode): seq[byte] {.inline.} = cast[seq[byte]](o)
 converter toChainCode*(s: seq[byte]): ChainCode {.inline.} = ChainCode(s)
-converter toBytes*(o: secp256k1_pubkey): seq[byte] {.inline.} =
-  (cast[array[64, byte]](o.data)).toSeq
-converter toPublicKeyObj*(o: secp256k1_pubkey): PublicKeyObj {.inline.} =
-  PublicKeyObj((cast[array[64, byte]](o.data)).toSeq)
-converter toSecp256k1Pubkey*(o: PublicKeyObj): secp256k1_pubkey {.inline.} =
-  cast[ptr secp256k1_pubkey](unsafeAddr (cast[ptr seq[byte]](unsafeAddr o))[0])[]
-
-var secp256k1_ctx* {.threadvar.}: ptr secp256k1_context
-
-proc init*() {.inline.} =
-  secp256k1_ctx = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY or SECP256K1_CONTEXT_SIGN)
-
-proc free*() {.inline.} =
-  secp256k1_context_destroy(secp256k1_ctx)
-
-proc ctx*(): ptr secp256k1_context =
-  if secp256k1_ctx.isNil:
-    init()
-  result = secp256k1_ctx
-
-proc ecPubKeyCreate*(privateKey: PrivateKey): secp256k1_pubkey =
-  if secp256k1_ec_pubkey_create(ctx(), addr result,
-                                cast[ptr cuchar](addr cast[ptr seq[byte]](unsafeAddr privateKey)[0])) == 0:
-    raise newException(EcError, "Impressive! verify privateKey failed")
-
-proc ecPubKeySerializeCompressed*(pubkey: secp256k1_pubkey): seq[byte] =
-  var publicKey = newSeq[byte](33)
-  var outputlen = publicKey.len.csize_t
-  if secp256k1_ec_pubkey_serialize(secp256k1_context_no_precomp, cast[ptr cuchar](addr publicKey[0]),
-                                  addr outputlen, unsafeAddr pubkey, SECP256K1_EC_COMPRESSED) == 0:
-    raise newException(EcError, "ecp256k1_ec_pubkey_serialize")
-  result = publicKey
-
-proc ecPubKeySerializeUncompressed*(pubkey: secp256k1_pubkey): seq[byte] =
-  var publicKey = newSeq[byte](65)
-  var outputlen = publicKey.len.csize_t
-  if secp256k1_ec_pubkey_serialize(secp256k1_context_no_precomp, cast[ptr cuchar](addr publicKey[0]),
-                                  addr outputlen, unsafeAddr pubkey, SECP256K1_EC_UNCOMPRESSED) == 0:
-    raise newException(EcError, "ecp256k1_ec_pubkey_serialize")
-  result = publicKey
-
-proc pubUncompressed*(privateKey: PrivateKey): PublicKey =
-  if privateKey.len != 32:
-    raise newException(EcError, "pub privateKey len=" & $privateKey.len)
-  var pubkey = ecPubKeyCreate(privateKey)
-  result = ecPubKeySerializeUncompressed(pubkey)
-
-proc pub*(privateKey: PrivateKey): PublicKey =
-  if privateKey.len != 32:
-    raise newException(EcError, "pub privateKey len=" & $privateKey.len)
-  var pubkey = ecPubKeyCreate(privateKey)
-  result = ecPubKeySerializeCompressed(pubkey)
-
-proc pub*(publicKeyObj: PublicKeyObj): PublicKey =
-  if publicKeyObj.len != 64:
-    raise newException(EcError, "pub publicKeyObj len=" & $publicKeyObj.len)
-  result = ecPubKeySerializeCompressed(publicKeyObj)
-
-proc pubObj*(privateKey: PrivateKey): PublicKeyObj =
-  if privateKey.len != 32:
-    raise newException(EcError, "pubObj privateKey len=" & $privateKey.len)
-  var pubkey = ecPubKeyCreate(privateKey)
-  result = pubkey.toBytes
-
-proc pubObj*(publicKey: PublicKey): PublicKeyObj =
-  var pubkey: secp256k1_pubkey
-  let srcPub = cast[ptr cuchar](unsafeAddr cast[ptr seq[byte]](unsafeAddr publicKey)[0])
-  if secp256k1_ec_pubkey_parse(secp256k1_context_no_precomp, addr pubkey,
-                              srcPub, publicKey.len.csize_t) == 0:
-    raise newException(EcError, "secp256k1_ec_pubkey_parse")
-  result = pubkey.toBytes
-
-proc sign*(privateKey: PrivateKey, hash32: openArray[byte], grind: bool = true): seq[byte] =
-  var sig: secp256k1_ecdsa_signature
-  let priv = cast[ptr cuchar](unsafeAddr cast[ptr seq[byte]](unsafeAddr privateKey)[0])
-  if secp256k1_ecdsa_sign(ctx(), addr sig, cast[ptr cuchar](unsafeAddr hash32[0]), priv,
-                          secp256k1_nonce_function_rfc6979, nil) != 1:
-    raise newException(EcError, "secp256k1_ecdsa_sign")
-  if grind and not cast[array[64, byte]](sig)[31] < 0x80.byte:
-    var ndata = newSeq[byte](32)
-    var counter: uint32 = 1
-    while true:
-      copyMem(addr ndata[0], addr counter, sizeof(counter))
-      if secp256k1_ecdsa_sign(ctx(), addr sig, cast[ptr cuchar](unsafeAddr hash32[0]), priv,
-                              secp256k1_nonce_function_rfc6979, addr ndata[0]) != 1:
-        raise newException(EcError, "secp256k1_ecdsa_sign")
-      if cast[array[64, byte]](sig)[31] < 0x80.byte:
-        break
-      inc(counter)
-  var der = newSeq[byte](75)
-  var derLen = 75.csize_t
-  if secp256k1_ecdsa_signature_serialize_der(ctx(), cast[ptr cuchar](addr der[0]), addr derLen, addr sig) != 1:
-    raise newException(EcError, "secp256k1_ecdsa_signature_serialize_der")
-  result = der[0..<derLen]
-
-proc verify*(publicKeyObj: PublicKeyObj, hash: openArray[byte], der: openArray[byte]): bool =
-  var sig: secp256k1_ecdsa_signature
-  var derLen = der.len.csize_t
-  if secp256k1_ecdsa_signature_parse_der(ctx(), addr sig, cast[ptr cuchar](unsafeAddr der[0]), derLen) != 1:
-    return false
-  secp256k1_ecdsa_signature_normalize(ctx(), addr sig, addr sig)
-  let pubkey = cast[ptr secp256k1_pubkey](unsafeAddr cast[ptr seq[byte]](unsafeAddr publicKeyObj)[0])
-  result = secp256k1_ecdsa_verify(ctx(), addr sig, cast[ptr cuchar](unsafeAddr hash[0]), pubkey) == 1
-
-proc verify*(publicKey: PublicKey, hash: openArray[byte], der: openArray[byte]): bool {.inline.} =
-  publicKey.pubObj.verify(hash, der)
 
 proc master*(seed: seq[byte], testnet: bool = false): HDNode =
   var I = sha512.hmac("Bitcoin seed", seed).data
@@ -218,24 +104,6 @@ proc node*(x: string, testnet: bool = false): HDNode =
     else:
       raise newException(HdError, "unknown version " & $ver.toBytesBE)
   result = node
-
-proc tweakAdd*(privateKey: PrivateKey, tweak: seq[byte]): PrivateKey =
-  if privateKey.len != 32 or tweak.len != 32:
-    raise newException(EcError, "tweakAdd privateKey len=" & $privateKey.len & " tweak len=" & $tweak.len)
-  var output: seq[byte] = privateKey
-  if secp256k1_ec_privkey_tweak_add(ctx(), cast[ptr cuchar](addr output[0]),
-                                    cast[ptr cuchar](unsafeAddr tweak[0])) == 0:
-    raise newException(EcError, "secp256k1_ec_privkey_tweak_add")
-  result = output
-
-proc tweakAdd*(publicKeyObj: PublicKeyObj, tweak: seq[byte]): PublicKeyObj =
-  if publicKeyObj.len != 64 or tweak.len != 32:
-    raise newException(EcError, "tweakAdd publicKeyObj len=" & $publicKeyObj.len & " tweak len=" & $tweak.len)
-  var output: seq[byte] = publicKeyObj
-  if secp256k1_ec_pubkey_tweak_add(ctx(), cast[ptr secp256k1_pubkey](addr output[0]),
-                                  cast[ptr cuchar](unsafeAddr tweak[0])) == 0:
-    raise newException(EcError, "secp256k1_ec_pubkey_tweak_add")
-  result = output
 
 proc hardened*(node: HDNode, index: uint32): HDNode =
   if node.privateKey.len != 32:
