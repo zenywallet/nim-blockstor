4ed14f00d8d06139be218ffa947986e959fc4a21
zenywallet <zenywallet@bitzeny.jp>
2021-10-21 12:08:21 +0900

    add zbar

---
+++ src/zbar.nim
@@ -0,0 +1,11 @@
+# cd deps/zbar
+# sed -i "s/ -Werror//" $(pwd)/configure.ac
+# autoreconf -vfi
+# emconfigure ./configure CPPFLAGS=-DNDEBUG=1 --without-x --without-jpeg --without-imagemagick --without-npapi --without-gtk --without-python --without-qt --without-xshm --disable-video --disable-pthread --enable-codes=all
+# emmake make
+# see https://github.com/Naahuel/zbar-wasm-barcode-reader
+
+import os
+
+const zbarPath = splitPath(currentSourcePath()).head & "/../deps/zbar"
+{.passL: zbarPath & "/zbar/.libs/libzbar.a".}
