00b494c0b60f849e031e98b08f60f3bf72b845b2
zenywallet <zenywallet@bitzeny.jp>
2022-01-20 01:45:56 +0900

    convert to cstring

--- src/otp_c.nim
+++ src/otp_c.nim
@@ -6,7 +6,7 @@ import otp, base32
 
 proc call_totp*(key: cstring, sec: uint64, digit: int = 6, timestep: int = 30, algo: int): cstring {.exportc.} =
   var totp = newTotp(base32.dec($key), digit, timestep, algo.ALGO)
-  result = totp.generate(sec)
+  result = totp.generate(sec).cstring
 
 #[
 char *call_totp(char* key, uint64_t sec, int digit, int timestep, int algo);
