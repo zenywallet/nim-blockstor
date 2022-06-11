ea68bc6b9a544f8c301cae340d54f4374af8b6da
zenywallet <zenywallet@bitzeny.jp>
2022-06-12 00:44:20 +0900

    change buffer seq to array

--- src/zenyjs/buffer.nim
+++ src/zenyjs/buffer.nim
@@ -1,6 +1,7 @@
 # Copyright (c) 2022 zenywallet
 
 import bytes {.all.}
+import arraylib
 
 type
   Buffer* = object
@@ -72,12 +73,25 @@ proc toBuffer*(x: seq[byte]): Buffer =
     copyMem(result.data, unsafeAddr x[0], x.len)
     result.len = x.len
 
-proc toBytes*(x: Buffer): seq[byte] =
+proc toBuffer*(x: Array[byte]): Buffer =
+  if x.len > 0:
+    result.data = cast[typeof(result.data)](allocShared0(x.len))
+    copyMem(result.data, unsafeAddr x[0], x.len)
+    result.len = x.len
+
+proc toBytes*(x: Buffer): Array[byte] =
+  result = newArray[byte](x.len)
+  for i in 0..<x.len:
+    result[i] = x[i]
+
+proc toSeq*(x: Buffer): seq[byte] =
   result = newSeq[byte](x.len)
   for i in 0..<x.len:
     result[i] = x[i]
 
-proc toBytesBE*(x: Buffer): seq[byte] {.inline.} = toBytes(x)
+proc toBytesBE*(x: Buffer): Array[byte] {.inline.} = toBytes(x)
+
+proc toSeqBE*(x: Buffer): seq[byte] {.inline.} = toSeq(x)
 
 when TOHEX_COMPACT:
   proc toHex*(a: Buffer): string =
