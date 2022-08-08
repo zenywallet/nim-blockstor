f83ea710717df4b1e1e205e67b7d673dfb459340
zenywallet <zenywallet@bitzeny.jp>
2022-08-08 17:52:51 +0900

    add jslevenshtein

---
+++ src/zenyjs/jslevenshtein.nim
@@ -0,0 +1,18 @@
+# Copyright (c) 2022 zenywallet
+
+import jsffi
+import os
+
+var module {.importc, nodecl.}: JsObject
+
+# https://github.com/gustf/js-levenshtein
+{.emit: staticRead(currentSourcePath().parentDir() / "../../deps/js-levenshtein/index.js").}
+
+var Module = JsObject{}
+Module.levenshtein = module.exports
+
+proc levenshtein*(a, b: cstring): int = Module.levenshtein(a, b).to(int)
+
+
+when isMainModule:
+  echo levenshtein("kitten", "sitting")
