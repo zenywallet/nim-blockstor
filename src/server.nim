950ca964151664757a1d7e0768f4a49658d1bc6a
zenywallet <zenywallet@bitzeny.jp>
2023-07-28 00:48:53 +0900

    Revert "fix uninitialized send buffer size"
    This reverts commit 9abd919edb408cb4aa97919fec4eeb371e44bfca.

--- src/server.nim
+++ src/server.nim
@@ -642,7 +642,6 @@ proc close(client: ptr Client) =
   if not client.recvBuf.isNil:
     deallocShared(cast[pointer](client.recvBuf))
     client.recvBuf = nil
-  clisent.sendBufSize = 0
   if not client.sendBuf.isNil:
     deallocShared(cast[pointer](client.sendBuf))
     client.sendBuf = nil
