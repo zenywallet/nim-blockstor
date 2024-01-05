c24670b822157a0b540e7911bb1c5a42b0ff5daf
zenywallet <zenywallet@bitzeny.jp>
2024-01-05 10:19:08 +0900

    change seed importc cint

--- src/seed.nim
+++ src/seed.nim
@@ -2,14 +2,14 @@
 
 include seed_native
 
-proc cryptSeed*(buf: ptr UncheckedArray[byte], size: int): int {.importc: "crypt_seed", cdecl.}
+proc cryptSeed*(buf: ptr UncheckedArray[byte], size: cint): cint {.importc: "crypt_seed", cdecl.}
 
 when not defined(js):
   proc cryptSeed*(buf: openArray[byte]): int {.inline.} =
-    cryptSeed(cast[ptr UncheckedArray[byte]](buf), buf.len)
+    cryptSeed(cast[ptr UncheckedArray[byte]](buf), buf.len.cint)
 
   proc cryptSeed*(size: int): seq[byte] =
     var a = newSeq[byte](size)
-    var ret = cryptSeed(cast[ptr UncheckedArray[byte]](addr a[0]), size)
+    var ret = cryptSeed(cast[ptr UncheckedArray[byte]](addr a[0]), size.cint)
     if ret == 0:
       result = a
