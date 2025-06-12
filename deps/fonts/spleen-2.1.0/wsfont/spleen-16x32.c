e086c3b2bb5d70b090329405bb6d2685725ceaaf
zenywallet <zenywallet@bitzeny.jp>
2025-06-13 01:28:27 +0900

    add deps fonts spleen

---
+++ deps/fonts/spleen-2.1.0/wsfont/spleen-16x32.c
@@ -0,0 +1,33 @@
+/*
+ * Spleen 2.1.0
+ * Copyright (c) 2018-2024, Frederic Cambus
+ * https://www.cambus.net/
+ *
+ * Created:      2020-06-20
+ * Last Updated: 2020-07-08
+ *
+ * Spleen is released under the BSD 2-Clause license.
+ * See LICENSE file for details.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+#include <dev/wscons/wsconsio.h>
+#include <dev/wsfont/spleen16x32.h>
+
+int
+main(int argc, char *argv[])
+{
+	size_t loop;
+
+	for (loop = 0; loop < 32 * 2 * 32; loop++)
+		printf("%c", 0);
+
+	for (loop = 0; loop < 224 * 2 * 32; loop++)
+		printf("%c", spleen16x32_data[loop]);
+
+	return 0;
+}
---
+++ deps/fonts/spleen-2.1.0/wsfont/spleen-5x8.c
@@ -0,0 +1,36 @@
+/*
+ * Spleen 2.1.0
+ * Copyright (c) 2018-2024, Frederic Cambus
+ * https://www.cambus.net/
+ *
+ * Created:      2020-06-20
+ * Last Updated: 2020-07-08
+ *
+ * Spleen is released under the BSD 2-Clause license.
+ * See LICENSE file for details.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+#include <dev/wscons/wsconsio.h>
+#include <dev/wsfont/spleen5x8.h>
+
+int
+main(int argc, char *argv[])
+{
+	size_t loop;
+
+	for (loop = 0; loop < 32 * 8; loop++)
+		printf("%c", 0);
+
+	for (loop = 0; loop < 96 * 8; loop++)
+		printf("%c", spleen5x8_data[loop]);
+
+	for (loop = 0; loop < 128 * 8; loop++)
+		printf("%c", 0);
+
+	return 0;
+}
